28b230a6fb465a3907efaa2c67a29e9fa06ee9ef
CLAUDIU-MIHAEL TERENCHE <135633780+Claudiu-Mihael-Terenche@users.noreply.github.com>
2024-02-09 23:53:32 -0500

    11:53/09.02.2024

--- Day01PeopleListInFile/Person.cs
+++ Day01PeopleListInFile/Person.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Day01PeopleListInFile
 {
     internal class Person
     {
+
+        public Person(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            City = city;
+        }
+
+        private string _name;
+        public string Name 
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 2 || value.Length > 100 || value.Contains(";"))
+                {
+                    throw new ArgumentException("Name must be 2 - 100 characters long, not containing semicolons");
+                }
+                _name = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 0 || value > 150 )
+                {
+                    throw new ArgumentException("Age must be 0-150");
+                }
+                _age = value;
+            }
+        }
+
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                // if (value.Length < 2 || value.Length > 100 || value.Contains(";"))
+                if (!Regex.IsMatch(value, @"^[^;]{2,100}$")) // , RegexOptions.IgnoreCase))
+                {
+                    throw new ArgumentException("City must be 2 - 100 characters long, not containing semicolons");
+                }
+                _city = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is {Age} from {City}";
+        }
     }
 }
--- Day01PeopleListInFile/Program.cs
+++ Day01PeopleListInFile/Program.cs
@@ -8,8 +8,116 @@ namespace Day01PeopleListInFile
 {
     internal class Program
     {
+        private const string DataFileName = @"..\..\people.txt";
+
+        static List<Person> People = new List<Person>();
+
+        private static int getMenuChoice()
+        {
+            try
+            {
+                Console.Write("What do you want to do?\n" + 
+                              "1. Add person info\n" + 
+                              "2. List persons info\n" + 
+                              "3. Find and list a person by name\n" + 
+                              "4. Find and list persons younger than age\n" + 
+                              "0. Exit\n" + "Choice: ");
+                string line = Console.ReadLine();
+                int choice = int.Parse(line); // ex
+                return choice;
+            }
+            catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+            {
+                return -1;
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Person p1 = new Person { Name = "John", Age = 33, City = "Montreal" };
+            ReadAllPeopleFromFile();
+            while (true)
+            {
+                int choice = getMenuChoice();
+                switch (choice)
+                {
+                    case 1:
+                        AddPersonInfo();
+                        break;
+                    case 2:
+                        ListAllPersonsInfo();
+                        break;
+                    case 3:
+                        FindPersonByName();
+                        break;
+                    case 4:
+                        FindPersonYoungerThan();
+                        break;
+                    case 0:
+                        SaveAllPeopleToFile();
+                        return; // exit the program
+                    default:
+                        Console.WriteLine("No such option, try again");
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        private static void SaveAllPeopleToFile()
+        {
+            // throw new NotImplementedException();
+        }
+
+        private static void FindPersonYoungerThan()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FindPersonByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ListAllPersonsInfo()
+        {
+            Console.WriteLine("Listing all persons");
+            foreach (Person person in People)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+        private static void AddPersonInfo()
+        {
+            try
+            {
+                Console.WriteLine("Adding a person.");
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine()); // ex. FormatException, OverflowException
+                Console.Write("Enter city: ");
+                string city = Console.ReadLine();
+                // Person person = new Person { Name = name, Age = age, City = city };
+                Person person = new Person(name, age, city); // ex ArgumentException
+                People.Add(person);
+                Console.WriteLine("Person added.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+            {
+                Console.WriteLine("Error: invalid numerical input");
+            }
+        }
+
+        private static void ReadAllPeopleFromFile()
+        {
+            // throw new NotImplementedException();
         }
     }
 }
